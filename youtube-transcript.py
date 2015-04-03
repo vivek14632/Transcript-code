b44c8e957641578bc2282ed4402c97eb995510b3
vivek14632 <viveks@comp.nus.edu.sg>
2015-04-03 21:55:32 +0800

    transcript working

---
+++ youtube-transcript.py
@@ -0,0 +1,36 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+fp=open('url.txt')
+urls=fp.readlines()
+fp.close()
+driver = webdriver.Firefox()
+
+for url in urls:
+	try:
+		url1=url.split('=')
+		url1[1]=url1[1].split('\n')
+	
+		out=open(url1[1][0],"wb")
+		'''driver = webdriver.Firefox()'''
+		'''driver = webdriver.PhantomJS()'''
+		driver.get(url)
+		k=driver.find_element_by_xpath("//*[@id='action-panel-overflow-button']")
+		k.send_keys('\n')
+		k=driver.find_element_by_xpath("//*[@data-trigger-for='action-panel-transcript']")
+		k.send_keys('\n')
+		t=driver.find_elements_by_xpath("//*[@class='caption-line-text']")
+		while(len(t)==0):
+			t=driver.find_elements_by_xpath("//*[@class='caption-line-text']")
+		for l in t:
+			out.write(l.text.encode('utf-8'))
+			print(l.text.encode('utf-8'))
+  
+		
+	except:
+		print("error")
+	out.close()
+driver.close()
+	
